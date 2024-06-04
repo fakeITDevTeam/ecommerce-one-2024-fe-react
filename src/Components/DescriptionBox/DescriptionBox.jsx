b900311eab1b1fc52960e70f91e0cff29469e324
fakeITDevTeam <dev5544455@gmail.com>
2024-06-04 12:58:13 +0700

    component: new: create descriptionBox of product

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+export const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores necessitatibus soluta ex, 
+          labore temporibus saepe in eveniet numquam expedita? Magni voluptate quam consequatur amet 
+          possimus! Laborum assumenda exercitationem natus temporibus nostrum minus cumque, 
+          fugiat deleniti voluptate vel excepturi culpa mollitia eos, repellendus distinctio necessitatibus. 
+          Itaque eos molestias veritatis unde, eius cumque iste pariatur, laboriosam, aut saepe quasi? 
+          Blanditiis, ducimus inventore?
+        </p>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nostrum aperiam, unde delectus 
+          aspernatur voluptatem at, eius natus est similique dignissimos quos ullam qui doloribus odio, 
+          laboriosam quod nihil exercitationem. Sapiente.
+        </p>
+      </div>
+    </div>
+  )
+}
